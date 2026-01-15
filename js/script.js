e4c34aeeebbfa3606150844b40fc4638e8415116
dcs3000 <dcs3000.507.2@gmail.com>
2026-01-15 15:43:31 -0300

    js ativando no scroll

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ const navMenu = document.querySelector('.nav-menu');
 menuToggle.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+
+const animatedElements = document.querySelectorAll('.animate');
+
+function animateOnScroll() {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  animatedElements.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < triggerBottom) {
+      el.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', animateOnScroll);
+animateOnScroll();
